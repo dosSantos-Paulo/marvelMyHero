9ae95a43fa0b42ae3eff6ceb46c85ff1d4838dde
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-11-16 23:40:52 -0300

    criado alertDialog para sair do app

--- app/src/main/java/com/example/marvelmyhero/view/MainActivity.kt
+++ app/src/main/java/com/example/marvelmyhero/view/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.marvelmyhero.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import com.example.marvelmyhero.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.ic_exit_main).setOnClickListener {
+            exitDialog()
+        }
+    }
+
+    private fun exitDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(getString(R.string.exitDialog_title))
+            .setMessage(getString(R.string.exitDialog_message))
+            .setNegativeButton(getString(R.string.exitDialog_negativeButton)) { _, _ ->
+                closeContextMenu()
+            }
+            .setPositiveButton(getString(R.string.exitDialog_positiveButton)) { _, _ ->
+                finish()
+            }
+            .show()
     }
 }
