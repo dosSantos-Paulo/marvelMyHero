d67aaa7f82ffe731f3679b9a87c77becb1cccba1
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-11-22 22:16:55 -0300

    criando recyclerView

--- app/src/main/java/com/example/marvelmyhero/model/Hero.kt
+++ app/src/main/java/com/example/marvelmyhero/model/Hero.kt
@@ -4,6 +4,7 @@ data class Hero (
     val id: Int,
     val heroName: String,
     val name: String,
+    val imageUrl: Int,
 
     val durability: Int,
     val energy: Int,
--- app/src/main/java/com/example/marvelmyhero/view/MyDeckActivity.kt
+++ app/src/main/java/com/example/marvelmyhero/view/MyDeckActivity.kt
@@ -3,7 +3,10 @@ package com.example.marvelmyhero.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.marvelmyhero.R
+import com.example.marvelmyhero.model.Hero
 
 class MyDeckActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +18,43 @@ class MyDeckActivity : AppCompatActivity() {
         arrowBack.setOnClickListener {
             finish()
         }
+
+        val cardRecyclerView = findViewById<RecyclerView>(R.id.recyclerView_myDeck)
+        val cardManager = GridLayoutManager(this, DECK_COLLUMN)
+        val cardAdapter = MyDeckAdapter(testCardColection(16))
+
+        cardRecyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = cardManager
+            adapter = cardAdapter
+        }
+
     }
+
+    companion object {
+        const val DECK_COLLUMN = 3
+
+        fun testCardColection (quantidade: Int) :MutableList<Hero> {
+            val cardList: MutableList<Hero> = mutableListOf()
+            for (i in 0 until quantidade) {
+                cardList.add(
+                    Hero(
+                        1,
+                        "Dr. Strange",
+                        "Stephen Strange",
+                        R.drawable.dr_strange,
+                        3,
+                        6,
+                        3,
+                        4,
+                        7,
+                        2,
+                        "Nothing"
+                    )
+                )
+            }
+            return cardList
+        }
+    }
+
 }
