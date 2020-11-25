c3e2365ee2490696ffc0051c7ee746892b56b3d8
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-11-25 17:37:26 -0300

    criando navegação para Main

--- app/src/main/java/com/example/marvelmyhero/login/view/LoginFragment.kt
+++ app/src/main/java/com/example/marvelmyhero/login/view/LoginFragment.kt
@@ -1,12 +1,15 @@
 package com.example.marvelmyhero.login.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.marvelmyhero.R
 import com.example.marvelmyhero.login.viewmodel.IMudarTab
+import com.example.marvelmyhero.view.MainActivity
 import com.google.android.material.textfield.TextInputEditText
 
 
@@ -20,6 +23,16 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_login_loginFragment).setOnClickListener {
+            val intent = Intent(view.context, MainActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 
 
