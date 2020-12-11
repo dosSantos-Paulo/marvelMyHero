1209bab2c6dfe30d10739d776c22d56a5cbb2a00
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-12-11 15:29:44 -0300

    criando splashScreenGif

---
+++ app/src/main/java/com/example/marvelmyhero/splash/view/SplashScreenGif.kt
@@ -0,0 +1,12 @@
+package com.example.marvelmyhero.splash.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.marvelmyhero.R
+
+class SplashScreenGif : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen_gif)
+    }
+}
