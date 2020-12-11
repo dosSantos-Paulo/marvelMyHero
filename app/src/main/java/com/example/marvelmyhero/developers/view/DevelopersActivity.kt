9c82bd60add64dfa6af16f51de63ae494c7587a0
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-12-11 16:33:16 -0300

    criando DevelopersActivity

---
+++ app/src/main/java/com/example/marvelmyhero/developers/view/DevelopersActivity.kt
@@ -0,0 +1,12 @@
+package com.example.marvelmyhero.developers.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.marvelmyhero.R
+
+class DevelopersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_developers)
+    }
+}
