2df8d9bd90e736df3e7198ccfbe770f2ed3d46fa
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-18 22:31:26 -0300

    validando banco de dados

--- app/src/main/java/com/example/marvelmyhero/db/dao/CardDao.kt
+++ app/src/main/java/com/example/marvelmyhero/db/dao/CardDao.kt
@@ -2,6 +2,7 @@ package com.example.marvelmyhero.db.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.marvelmyhero.db.entity.CardEntity
 
 @Dao
@@ -9,5 +10,14 @@ interface CardDao {
 
     @Insert
     suspend fun addCard(card: CardEntity)
+
+    @Query("SELECT COUNT(*) FROM Card")
+    suspend fun count(): Int
+
+    @Query("SELECT * FROM Card WHERE id = :id")
+    suspend fun getCard(id: Int): CardEntity
+
+    @Query("SELECT * FROM Card")
+    suspend fun getAllCards(): List<CardEntity>
 
 }
