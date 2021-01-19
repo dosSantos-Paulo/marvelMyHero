e2205937002c388e8ce7e77ab9f65c042e0b4f9b
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-19 15:38:10 -0300

    adicionando foto do device

--- app/src/main/java/com/example/marvelmyhero/register/RegisterActivity.kt
+++ app/src/main/java/com/example/marvelmyhero/register/RegisterActivity.kt
@@ -1,33 +1,60 @@
 package com.example.marvelmyhero.register
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.Toast
+import android.widget.VideoView
 import com.example.marvelmyhero.R
 import com.example.marvelmyhero.main.view.MainActivity
+import com.example.marvelmyhero.utils.Constants.CONTEXT_RESQUEST_CODE
 import com.google.android.material.textfield.TextInputEditText
 
 class RegisterActivity : AppCompatActivity() {
+
+    private var imageUri: Uri? = null
+
+    private val userImage: ImageView by lazy { findViewById(R.id.image_userImage_register) }
+
+    private val nickname: TextInputEditText by lazy { findViewById(R.id.editText_nickName_register) }
+
+    private val name: TextInputEditText by lazy { findViewById(R.id.editText_name_register) }
+
+    private val submitButton: Button by lazy { findViewById(R.id.btn_submit_register) }
+
+    private val changeImageButton: ImageView by lazy { findViewById(R.id.image_editIcon_register) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val nickName = findViewById<TextInputEditText>(R.id.editText_nickName_register)
-        val name = findViewById<TextInputEditText>(R.id.editText_name_register)
-        val submitButton = findViewById<Button>(R.id.btn_submit_register)
-
-        val changeImageButton = findViewById<ImageView>(R.id.image_editIcon_register)
-
         submitButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java )
-            startActivity(intent)
+            startActivity(Intent(this, MainActivity::class.java ))
         }
 
         changeImageButton.setOnClickListener {
-            Toast.makeText(this, "try", Toast.LENGTH_LONG).show()
+            findFile()
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CONTEXT_RESQUEST_CODE && resultCode == RESULT_OK) {
+            imageUri = data?.data
+            userImage.setImageURI(imageUri)
+        }
+        
+    }
+
+    private fun findFile() {
+
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, CONTEXT_RESQUEST_CODE)
+
     }
 }
--- app/src/main/java/com/example/marvelmyhero/utils/Constants.kt
+++ app/src/main/java/com/example/marvelmyhero/utils/Constants.kt
@@ -8,6 +8,8 @@ object Constants {
     const val UIID_KEY = "UIID"
     const val EMPTY_STRING = ""
 
+    const val CONTEXT_RESQUEST_CODE = 1
+
 //    Handler Time
 
     const val HANDLER_TIME: Long = 9000
