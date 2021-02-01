ad944731c511009112add85007a514d6e6a1aa90
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-02-01 20:14:43 -0300

    corrigindo bug de userFragment

--- app/src/main/java/com/example/marvelmyhero/main/view/UserFragment.kt
+++ app/src/main/java/com/example/marvelmyhero/main/view/UserFragment.kt
@@ -6,10 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import com.example.marvelmyhero.R
 import com.example.marvelmyhero.login.model.User
+import com.google.android.material.card.MaterialCardView
 import com.squareup.picasso.Picasso
 
 class UserFragment(
@@ -24,16 +26,22 @@ class UserFragment(
         return inflater.inflate(R.layout.fragment_user, container, false)
     }
 
-    @SuppressLint("ClickableViewAccessibility", "UseRequireInsteadOfGet")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
         super.onViewCreated(view, savedInstanceState)
-        requireView().setOnTouchListener { view, motionEvent -> return@setOnTouchListener true
-        }
 
+        val frameLayout = view.findViewById<FrameLayout>(R.id.frameLayout_userFragment)
+        val cardView = view.findViewById<MaterialCardView>(R.id.cardView_userFragment)
         val userImage = view.findViewById<ImageView>(R.id.img_user_fragmentUser)
         val userName = view.findViewById<TextView>(R.id.txt_userName_fragmentUser)
         val totalCard = view.findViewById<TextView>(R.id.txt_totalCards_fragmentUser)
+
+        cardView.setOnClickListener {
+            val nothing = true
+        }
+
+        frameLayout.setOnClickListener {
+            activity?.onBackPressed()
+        }
 
         Picasso.get().load(_user.imageUrl).into(userImage)
         userName.text = _user.nickName
