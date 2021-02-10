09ef1f06fb3b6ba4901e8d93779fc370973c22c7
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-02-09 21:22:07 -0300

    modificando pergunta para gravar arquivo

--- app/src/main/java/com/example/marvelmyhero/main/view/MainActivity.kt
+++ app/src/main/java/com/example/marvelmyhero/main/view/MainActivity.kt
@@ -117,7 +117,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         shareButton.setOnClickListener {
-            checkPermissionREAD_EXTERNAL_STORAGE(this)
             share()
         }
     }
@@ -142,67 +141,6 @@ class MainActivity : AppCompatActivity() {
             )
         }
         return cardFirebase
-    }
-
-    val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123
-
-    fun checkPermissionREAD_EXTERNAL_STORAGE(
-        context: Context?
-    ): Boolean {
-        val currentAPIVersion = Build.VERSION.SDK_INT
-        return if (currentAPIVersion >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                ) !== PackageManager.PERMISSION_GRANTED
-            ) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(
-                        this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE
-                    )
-                ) {
-                    showDialog(
-                        "External storage", context,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE
-                    )
-                } else {
-                    ActivityCompat
-                        .requestPermissions(
-                            this,
-                            arrayOf<String>(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                            MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
-                        )
-                }
-                false
-            } else {
-                true
-            }
-        } else {
-            true
-        }
-    }
-
-    fun showDialog(
-        msg: String, context: Context?,
-        permission: String
-    ) {
-        val alertBuilder: AlertDialog.Builder = AlertDialog.Builder(context)
-        alertBuilder.setCancelable(true)
-        alertBuilder.setTitle("Permission necessary")
-        alertBuilder.setMessage("$msg permission is necessary")
-        alertBuilder.setPositiveButton(android.R.string.yes,
-            object : DialogInterface.OnClickListener {
-                override fun onClick(dialog: DialogInterface?, which: Int) {
-                    share()
-                    ActivityCompat.requestPermissions(
-                        (context as Activity?)!!, arrayOf(permission),
-                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
-                    )
-                }
-
-            })
-        val alert: AlertDialog = alertBuilder.create()
-        alert.show()
     }
 
     private fun initDbViewModel() {
--- app/src/main/java/com/example/marvelmyhero/splash/view/SplashScreen.kt
+++ app/src/main/java/com/example/marvelmyhero/splash/view/SplashScreen.kt
@@ -1,6 +1,13 @@
 package com.example.marvelmyhero.splash.view
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -9,6 +16,8 @@ import android.util.Log
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
 import com.example.marvelmyhero.R
@@ -69,6 +78,8 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        checkPermissionREAD_EXTERNAL_STORAGE(this)
+
         databaseViewModel = ViewModelProvider(
             this,
             CardViewModel.CardViewModelFactory(
@@ -241,6 +252,66 @@ class SplashScreen : AppCompatActivity() {
 
 
         callNextPage()
+    }
+
+    val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123
+
+    fun checkPermissionREAD_EXTERNAL_STORAGE(
+        context: Context?
+    ): Boolean {
+        val currentAPIVersion = Build.VERSION.SDK_INT
+        return if (currentAPIVersion >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) !== PackageManager.PERMISSION_GRANTED
+            ) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                ) {
+                    showDialog(
+                        "External storage", context,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                } else {
+                    ActivityCompat
+                        .requestPermissions(
+                            this,
+                            arrayOf<String>(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                            MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
+                        )
+                }
+                false
+            } else {
+                true
+            }
+        } else {
+            true
+        }
+    }
+
+    fun showDialog(
+        msg: String, context: Context?,
+        permission: String
+    ) {
+        val alertBuilder: AlertDialog.Builder = AlertDialog.Builder(context)
+        alertBuilder.setCancelable(true)
+        alertBuilder.setTitle("Permission necessary")
+        alertBuilder.setMessage("$msg permission is necessary")
+        alertBuilder.setPositiveButton(android.R.string.yes,
+            object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    ActivityCompat.requestPermissions(
+                        (context as Activity?)!!, arrayOf(permission),
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
+                    )
+                }
+
+            })
+        val alert: AlertDialog = alertBuilder.create()
+        alert.show()
     }
 
 
